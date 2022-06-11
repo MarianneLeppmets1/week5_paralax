e6c1c3c8e7e521749e9a62aecb80307fff7f5d91
MarianneLeppmets1 <105100062+MarianneLeppmets1@users.noreply.github.com>
2022-06-11 13:58:53 +0300

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+let now = new Date();
+console.log (now);
+
+let year = now-getFullYear();
+console.log(year)
+
+let yearSpan=document.querySelector("year");
+consele.log(YearSpan);
+YearSpan.innerText = year;
